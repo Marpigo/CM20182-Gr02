db65c5471ab6da1261f0d481acb23f1ef5eb6a01
Marpigo <marpigov@gmail.com>
2018-08-25 15:53:50 -0500

    inicio activity comidas

--- Lab1-UI/src/main/java/co/edu/udea/compumovil/gr02_20182/lab1/ComidaActivity.java
+++ Lab1-UI/src/main/java/co/edu/udea/compumovil/gr02_20182/lab1/ComidaActivity.java
@@ -3,7 +3,8 @@ package co.edu.udea.compumovil.gr02_20182.lab1;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class ComidaActivity extends AppCompatActivity {
+public class ComidaActivity extends AppCompatActivity{
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
