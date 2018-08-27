59a74a166b58a5b98f207f340818afc91a06a05a
EDWARMARTINEZ <edwar.martinez@udea.edu.co>
2018-08-26 20:38:00 -0500

    1.Actualizacion del activity Comidad 2.Commit despues de montar nuevamente el sistema operativo

--- Lab1-UI/src/main/java/co/edu/udea/compumovil/gr02_20182/lab1/ComidaActivity.java
+++ Lab1-UI/src/main/java/co/edu/udea/compumovil/gr02_20182/lab1/ComidaActivity.java
@@ -2,13 +2,51 @@ package co.edu.udea.compumovil.gr02_20182.lab1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.app.TimePickerDialog;
+import android.widget.TextView;
+import android.widget.TimePicker;
 
-public class ComidaActivity extends AppCompatActivity{
+import java.util.Calendar;
+
+public class ComidaActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button bhora;
+    TextView ttiemo;
+    private int hora, minuto;
+
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        this.setTitle(R.string.mi_titulo2);
         setContentView(R.layout.activity_comida);
+
+        bhora = (Button)findViewById(R.id.btHora);
+        ttiemo = (TextView)findViewById(R.id.txtTiempo);
+        bhora.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v ==bhora){
+            final Calendar calendario = Calendar.getInstance();
+            hora = calendario.get(Calendar.HOUR_OF_DAY);
+            minuto = calendario.get(Calendar.MINUTE);
+
+
+            TimePickerDialog timePickerDialog = new TimePickerDialog(this, new TimePickerDialog.OnTimeSetListener() {
+                @Override
+                public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                    ttiemo.setText(hourOfDay+":"+minute);
+                }
+            },hora,minuto,false);
+            timePickerDialog.show();
+
+        }
     }
 }
